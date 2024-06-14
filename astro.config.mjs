d87f9f371c1aff73ad6aa7ad0667bc4abd3b65a6
Thomas Allen <71492408+teasmade@users.noreply.github.com>
2024-06-14 14:20:24 +0200

    add preact component demo, rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://cheery-kashata-d8db32.netlify.app",
+  integrations: [preact()]
+});
